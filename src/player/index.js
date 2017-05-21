f51b2aa8f7bd14dc30c8464edbd2bcf2ec5f0f96
Stan Kudrow <st4sik@gmail.com>
2017-05-21 18:58:00 -0400

    player.toJSON

--- src/player/index.js
+++ src/player/index.js
@@ -1,7 +1,19 @@
 export default class Player {
-  constructor({ id, cards = [] })
-   {
+  constructor({ id, cards = [], status }) {
     this.id = id;
     this.cards = cards;
+    this.status = status;
+  }
+
+  toJSON() {
+    const json = {};
+    for (const prop in this) {
+      if (prop === 'cards') {
+        json[prop] = this[prop].map(card => card.toString())
+      } else {
+        json[prop] = this[prop];  
+      }
+    }
+    return json;
   }
 }
